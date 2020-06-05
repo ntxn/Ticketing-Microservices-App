ab88fbaeacec0c77a91ad424cf19781810463506
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-06-05 10:33:18 -0700

    Expiration: Initial setup

---
+++ expiration/src/index.ts
@@ -0,0 +1,30 @@
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+  if (!process.env.NATS_CLUSTER_ID)
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  if (!process.env.NATS_CLIENT_ID)
+    throw new Error('NATS_CLIENT_ID must be defined');
+  if (!process.env.NATS_URL) throw new Error('NATS_URL must be defined');
+
+  try {
+    // Connect to Nats streaming service
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    // Prepare for Nats to gracefully shutdown
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
