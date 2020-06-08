5399abc389ad33dfa3d5a88f7503be9f1943b1ff
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-06-08 07:54:58 -0700

    Payments: Created order created and cancelled event listeners to add and update orders in payment service

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,28 @@
+import {
+  Listener,
+  Subjects,
+  OrderCancelledEvent,
+  NotFoundError,
+  OrderStatus,
+} from '@2ntickets/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+    const order = await Order.findOne({
+      _id: data.id,
+      version: data.version - 1,
+    });
+    if (!order) throw new NotFoundError();
+
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+
+    msg.ack();
+  }
+}
