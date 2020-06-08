2f92d9a2325c53678b2a5c05bc5eff7eff7c47d4
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-06-08 07:46:17 -0700

    Payments: Initial setup - install dependencies, setup express server, nats, docker image, Deployment, Service, DB and ingress connection

---
+++ payments/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+  requireAuth,
+} from '@2ntickets/common';
+
+import { createChargeRouter } from './routes';
+
+const app = express();
+
+app.set('trust proxy', true);
+
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+app.use(requireAuth);
+
+app.use(createChargeRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/index.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+import {
+  OrderCreatedListener,
+  OrderCancelledListener,
+} from './events/listeners';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
+  if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined');
+  if (!process.env.NATS_CLUSTER_ID)
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  if (!process.env.NATS_CLIENT_ID)
+    throw new Error('NATS_CLIENT_ID must be defined');
+  if (!process.env.NATS_URL) throw new Error('NATS_URL must be defined');
+
+  try {
+    // Connect to Nats streaming service
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    // Prepare for Nats to gracefully shutdown
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
+
+    // Connect to Mongo DB
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => console.log('PAYMENTS listening on port 3000'));
+};
+
+start();
